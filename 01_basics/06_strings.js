224729b04f99459b627d50794e8a37252eaefa29
Ganesh Darewar <71462162+ganeshdarewar@users.noreply.github.com>
2023-09-22 14:00:33 +0000

    modified-Strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -11,21 +11,26 @@ const userName=new String("ganesh")// another way to define strngs which support
 //     userName.toUpperCase(), 
 //     userName[0], 
 //     userName.__proto__, 
-//     userName.charAt(3),
+//     userName.charAt(0),
 //     userName.indexOf('g')
 // ]);
 
 // const newString=userName.substring(0,4)
 // console.log(newString);
 
-// const anotherString=userName.substring(-1,5)
-// console.log(anotherString);
+/*
+The substring() method swaps its two arguments if indexStart is greater than indexEnd, meaning that a string is still returned
+If either or both of the arguments are negative or NaN, the substring() method treats them as if they were 0.
+*/
+
+const otherName=userName.slice(-5,2)
+console.log(otherName);
 
 // const newStringOne=userName.trim()
 // console.log(newStringOne);
 
 const url="ganesh%20darewaar.com"
 
-console.log(url.replace('%20', '-'));
-console.log(url.includes('hitesh'));
-console.log(url.split('e'));
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('hitesh'));
+// console.log(url.split('e'));
