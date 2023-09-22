1f90b619f32915571104b0635f7ae0a49223a60e
Ganesh Darewar <71462162+ganeshdarewar@users.noreply.github.com>
2023-09-22 06:45:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID=123
+let accountName="Ganesh"
+var accountNumber=234
+accountEmail="ganeshdarewar@gmail.com"
+let accountState
+
+// accountID=1234 // not allowed
+
+/*
+prefer not to use var because of the block scope or functional scope
+*/
+accountName="Hitesh"
+
+console.table([accountID, accountName, accountNumber, accountEmail, accountState])
