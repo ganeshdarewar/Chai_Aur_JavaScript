5d9ae97fe3ccc1041f65eecb50ba47d620ebd4d5
Ganesh Darewar <71462162+ganeshdarewar@users.noreply.github.com>
2023-09-22 06:46:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js as newer version
+
+
+/*
+Datatypes
+
+number => 2^53 storage
+bigInt
+string
+boolean
+null=> standalone value
+undefined
+symbol => unique
+
+*/
+
+let a=23
+let b="Ganesh"
+let c=false
+let d=null
+let e;
+
+console.table([typeof(a), typeof(b), typeof(c), typeof(d), typeof(e)] );
