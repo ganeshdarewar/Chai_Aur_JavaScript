b8c7de035a4366c63d40c0233bfe65bfd0a28c9b
Ganesh Darewar <71462162+ganeshdarewar@users.noreply.github.com>
2023-09-22 07:15:17 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score=null;
+
+let valueInNmber=Number(score)
+
+// console.log(valueInNmber);
+// console.log(typeof valueInNmber);
+
+// "33abc" =>NaN = not a number
+// "" => 0
+// 33 => 33
+// null => 0
+// true => 1, false=> 0
+
+let isLogIn=""
+
+let booleanIsLogIn=Boolean(isLogIn)
+console.log(booleanIsLogIn);
+console.log(typeof booleanIsLogIn);
+
+
+//  "ganesh", "0", "1" , "22sd"=> true
+// null, "", => false
+
+
+
